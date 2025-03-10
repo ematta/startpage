5dca2f626aace24dca3a5b523e95a7d551eb778c
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-03-10 08:39:55 -0500

    init

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+export default function Home() {
+  return (
+    <>
+      <div>Hello world! This is Enrique Matta's page. It's bland, just like I like it.</div>
+      <div id="links">
+        <a href="blog.matta.dev">Blog</a> <br />
+        <a href="github.com/ematta">GITHUB</a> <br />
+        <a href="linkedin.com/in/enriquematta">Linkedin</a> <br />
+      </div>
+    </>
+  )
+}
